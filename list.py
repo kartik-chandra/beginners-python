0f989f452a92f2ca3ae7ba0e47f64beeb5d42985
onsaurav <kartik.chandra@wundermanthompson.com>
2023-11-21 15:20:26 +0600

    Updated in main

--- list.py
+++ list.py
@@ -30,6 +30,41 @@ print(cars)
 
 newcars = sorted(cars)
 print(newcars)
+print(cars)
+
+cars = ["Ford", "Toyota", "Corola", "BMW"]
+print(cars)
+print(cars[2])
+
+cars.append("Aleon")
+print(cars)
+
+cars.insert(0, "Honda")
+print(cars)
+cars.insert(3, "Mahindra")
+print(cars)
+
+newCar = cars.pop()
+print(newCar)
+print(cars)
+
+thirdCar = cars.pop(2)
+print(thirdCar)
+print(cars)
+
+extraCar = "Honda"
+cars.remove(extraCar)
+print(cars)
+
+cars.sort()
+print(cars)
+
+cars.sort(reverse = True)
+print(cars)
+
+newcars = sorted(cars)
+print(newcars)
+print(cars)
 
 cars.reverse()
 print(cars)
