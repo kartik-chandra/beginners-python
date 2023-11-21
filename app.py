ffc3a5dc4049ed076f68734c6e744c7327bd9636
onsaurav <kartik.chandra@wundermanthompson.com>
2023-11-21 11:51:41 +0600

    String operations added

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 print("Python for Beginners!")
+
+#This section is about string
+
+str = "This section is all about string"
+print(str)
+
+name = "Saurav"   #\n \t are use for new line and tab anf f is used at the start of a string to format string
+print( f"Hi {name}, \nWelcome here")
+
+#cases of string
+mytest = "It is just a text to test all cases"
+print("original: " + mytest.title())
+print("title: " + mytest.title())
+print("upper: " + mytest.upper())
+print("lower: " + mytest.lower())
+
