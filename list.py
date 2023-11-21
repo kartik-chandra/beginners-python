2db6eaf1999b5ad7b6e10cb6e6c0d290089edbaa
onsaurav <kartik.chandra@wundermanthompson.com>
2023-11-21 16:12:47 +0600

    list operations added

--- list.py
+++ list.py
@@ -69,4 +69,62 @@ print(cars)
 cars.reverse()
 print(cars)
 
+print(cars[-2])
+
 print(f"Total number of cars: {len(cars)}")
+
+#loop
+for car in cars:
+    print(car)
+
+for i in range(1, 5):
+    print(i)
+    
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
+
+squars = []
+for val in range (2, 11, 2):
+    squar = val ** 2
+    squars.append(squar)
+
+print(squars)
+print(min(squars))
+print(max(squars))
+print(sum(squars))
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players)
+print(players[0:3])
+print(players[1:4])
+print(players[2:])
+print(players[-3:])
+
+
+
+# Example List
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Get elements from index 2 to 5 (not including 5)
+sliced_list = my_list[2:5]
+print(sliced_list)  # Output: [2, 3, 4]
+
+# Get elements from the beginning to index 5 (not including 5)
+sliced_list = my_list[:5]
+print(sliced_list)  # Output: [0, 1, 2, 3, 4]
+
+# Get elements from index 2 to the end
+sliced_list = my_list[2:]
+print(sliced_list)  # Output: [2, 3, 4, 5, 6, 7, 8, 9]
+
+# Get the entire list (a copy of the original list)
+sliced_list = my_list[:]
+print(sliced_list)  # Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Using negative indices
+sliced_list = my_list[-3:]  # Last three elements
+print(sliced_list)  # Output: [7, 8, 9]
+
+# Get every second element from index 1 to 7
+sliced_list = my_list[1:8:2]
+print(sliced_list)  # Output: [1, 3, 5, 7]
